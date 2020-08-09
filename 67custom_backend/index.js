4976910c2760c83263db4ee8edf233e5b58c7cc1
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2020-08-09 22:56:56 +0530

    Learning Nodejs.
    Creating responsive HTML website with nodejs.

--- 67custom_backend/index.js
+++ 67custom_backend/index.js
@@ -5,10 +5,21 @@ const fs = require('fs');
 // visit : https://nodejs.org/en/about/
 const hostname = '127.0.0.1';
 const port = 3000;
-const Home = fs.readFileSync('index.html');
-const About = fs.readFileSync('About.html');
-const Contacts = fs.readFileSync('Contacts.html');
-const Carrers = fs.readFileSync('Carrers.html');
+const home = fs.readFileSync('index.html');  /*Created in same folders, so that you can */
+const about = fs.readFileSync('./about.html'); /*directly mention  name of that file*/
+const contacts = fs.readFileSync('./contact.html');
+const services = fs.readFileSync('./services.html');
+
+// I could able find Imp error after splitting screen and compare with another code.
+
+// Important: Below you can see 4 lines, where I was getting error
+// because I have used different variables here and I was using something
+// different variable in programs
+// const Home = fs.readFileSync('index.html');  
+// const About = fs.readFileSync('./about.html');
+// const Contacts = fs.readFileSync('./contact.html');
+// const Carrers = fs.readFileSync('./services.html');
+// 'Home' is declared but its value is never read.
 
 // arrow function
 
@@ -28,10 +39,26 @@ const Carrers = fs.readFileSync('Carrers.html');
 
 const server = http.createServer((req,res)=>{
   console.log(req.url)
+  url = req.url; 
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('Hello World');
+  if(url == '/'){
+    res.end(home)
+  }
+  else if(url == '/about'){
+    res.end(About)
+  }
+  else if(url == '/contact'){
+    res.end(contact)
+  }
+  else if(url == '/services'){
+    res.end(services)
+  }
+  else{
+    res.statusCode = 404;
+    res.end('404 not found')
+  }
 });
 
 // we will put html template in res.end(``)
